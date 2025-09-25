331a1e612af60762f30a4e1e623cc659d1464fc0
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-09-25 14:18:35 +0300

    ChatBot 4st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -4,29 +4,33 @@ public class ChatBot {
     public static void main(String[] args) {
         String botName = "my bot"; // ім’я бота
         int birthYear = 2025;
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
         System.out.println("Please, remind me your name.");
         System.out.print("> ");
-
-        Scanner scanner = new Scanner(System.in);
         String yourName = scanner.nextLine();
-
         System.out.println("What a great name you have, " + yourName + "!");
         System.out.println();
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
-
         System.out.print("> ");
         int rem3 = scanner.nextInt();
         System.out.print("> ");
         int rem5 = scanner.nextInt();
         System.out.print("> ");
         int rem7 = scanner.nextInt();
-
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
-
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println();
+
+        System.out.println("Now I will prove to you that I can count to any number you want!");
+        System.out.print("> ");
+        int num = scanner.nextInt();
+
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i + "!");
+        }
     }
 }
