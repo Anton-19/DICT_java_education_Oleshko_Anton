19aa4a30df00580779ff8a1751fb4481c7356bf3
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-09-25 14:25:23 +0300

    ChatBot 5st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -28,9 +28,28 @@ public class ChatBot {
         System.out.println("Now I will prove to you that I can count to any number you want!");
         System.out.print("> ");
         int num = scanner.nextInt();
-
         for (int i = 0; i <= num; i++) {
             System.out.println(i + "!");
         }
+        System.out.println();
+
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times");
+        System.out.println("2. To decompose a program into small subroutines");
+        System.out.println("3. To determine the execution time of a program");
+        System.out.println("4. To interrupt the execution of a program");
+
+        int answer;
+        do {
+            System.out.print("> ");
+            answer = scanner.nextInt();
+            if (answer != 2) {
+                System.out.println("Please, try again.");
+            }
+        } while (answer != 2);
+
+        System.out.println("Congratulations, you passed the test!");
+        System.out.println("Goodbye, have a nice day!");
     }
 }
