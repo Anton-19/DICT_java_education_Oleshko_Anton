388c503da18b6a4fa69f4639eae3fb9d894e4c8a
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-09-25 14:11:41 +0300

    ChatBot 2st stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
-        String botName = "my bot"; // будь-яке ім’я для бота
+        String botName = "my bot"; // ім’я бота
         int birthYear = 2025;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+        System.out.print("> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String yourName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + yourName + "!");
     }
 }
