cee90d630285210fc27a64c38b0608909fe5bd2b
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-10-02 14:08:37 +0300

    Hangman 2-st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
+
 public class Hangman {
 
     // Функція для виводу вітального повідомлення
     public static void printAnnouncement() {
-        System.out.println("     HANGMAN GAME      ");
-        System.out.println("The game will be available soon.");
+        System.out.println("HANGMAN");
     }
 
+    // Функція для початку гри
+    public static void startGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        String secretWord = "java";              // заздалегідь встановлене слово
+        System.out.print("Guess the word: > ");
+        String guess = scanner.nextLine();
+
+        if (guess.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+    }
+
+    // Основне меню
     public static void mainMenu() {
         printAnnouncement();
+        startGame();
     }
 
+    // Головна функція
     public static void main(String[] args) {
         mainMenu();
     }
