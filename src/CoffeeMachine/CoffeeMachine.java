41cc204783bd424b583478962ed6ca75d8ce32ff
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-10-13 19:12:26 +0300

    CoffeeMachine3-nd stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -3,25 +3,58 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many cups of coffee you will need:");
-        System.out.print("> ");
-        int cups = scanner.nextInt();
+        // Введення даних
+        int water = Ingredient(scanner, "water");
+        int milk = Ingredient(scanner, "milk");
+        int beans = Ingredient(scanner, "coffee beans");
+        int cupsNeeded = Cups(scanner);
 
-        // Розрахунок інгредієнтів
-        int water = cups * 200; // мл води
-        int milk = cups * 50;   // мл молока
-        int beans = cups * 15;  // г кавових зерен
-
-        // Вивід результатів
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(beans + " g of coffee beans");
+        // Розрахунок і вивід результату
+        printResult(water, milk, beans, cupsNeeded);
 
         scanner.close();
+    }
+
+    //   запит інгредієнтів
+    private static int Ingredient(Scanner scanner, String ingredient) {
+        System.out.println("Write how many ml of " + ingredient + " the coffee machine has:");
+        System.out.print("> ");
+        return scanner.nextInt();
+    }
+
+    //  запит кількості чашок
+    private static int Cups(Scanner scanner) {
+        System.out.println("\nWrite how many cups of coffee you will need:");
+        System.out.print("> ");
+        return scanner.nextInt();
+    }
+
+    //  розрахунок скільки чашок можна приготувати
+    private static int calculateCups(int water, int milk, int beans) {
+        int waterPerCup = 200;
+        int milkPerCup = 50;
+        int beansPerCup = 15;
+        return Math.min(
+                Math.min(water / waterPerCup, milk / milkPerCup),
+                beans / beansPerCup
+        );
+    }
+
+    //  вивод результату
+    private static void printResult(int water, int milk, int beans, int cupsNeeded) {
+        int possibleCups = calculateCups(water, milk, beans);
+
+        if (cupsNeeded == possibleCups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (cupsNeeded < possibleCups) {
+            int extra = possibleCups - cupsNeeded;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + possibleCups + " cups of coffee");
+        }
     }
 }
