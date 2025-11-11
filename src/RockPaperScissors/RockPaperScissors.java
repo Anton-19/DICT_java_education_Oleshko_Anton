8913f7dd8cfb62a015171419c3f7296ac3cd4d60
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-11-11 11:08:37 +0200

    RockPaperScissors 4-th stage

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,46 +1,102 @@
 package RockPaperScissors;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class RockPaperScissors {
+
+    private static final String[] OPTIONS = {"rock", "paper", "scissors"};
+    private static final Random random = new Random();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
 
-        String[] options = {"rock", "paper", "scissors"};
+        //Зчитування імені користувача
+        System.out.print("Enter your name: > ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
 
+        //Завантаження рейтингу користувача
+        int rating = loadRating(name);
+
+        //Основний ігровий цикл
         while (true) {
             System.out.print("> ");
             String userChoice = scanner.nextLine().toLowerCase();
 
-            // Перевіряємо, чи користувач хоче вийти
             if (userChoice.equals("!exit")) {
                 System.out.println("Bye!");
                 break;
-            }
-
-            // Перевіряємо правильність введення
-            if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+            } else if (userChoice.equals("!rating")) {
+                System.out.println("Your rating: " + rating);
+                continue;
+            } else if (!isValidOption(userChoice)) {
                 System.out.println("Invalid input");
                 continue;
             }
 
-            // Випадковий вибір комп'ютера
-            String computerChoice = options[random.nextInt(3)];
+            // Хід комп'ютера
+            String computerChoice = OPTIONS[random.nextInt(OPTIONS.length)];
 
             // Визначення результату
-            if (userChoice.equals(computerChoice)) {
-                System.out.println("There is a draw (" + computerChoice + ")");
-            } else if (
-                    (userChoice.equals("rock") && computerChoice.equals("scissors")) ||
-                            (userChoice.equals("scissors") && computerChoice.equals("paper")) ||
-                            (userChoice.equals("paper") && computerChoice.equals("rock"))
-            ) {
-                System.out.println("Well done. The computer chose " + computerChoice + " and failed");
-            } else {
-                System.out.println("Sorry, but the computer chose " + computerChoice);
+            String result = getResult(userChoice, computerChoice);
+
+            // Виведення результату гри
+            switch (result) {
+                case "win":
+                    System.out.println("Well done. The computer chose " + computerChoice + " and failed");
+                    rating += 100;
+                    break;
+                case "draw":
+                    System.out.println("There is a draw (" + computerChoice + ")");
+                    rating += 50;
+                    break;
+                case "lose":
+                    System.out.println("Sorry, but the computer chose " + computerChoice);
+                    break;
             }
         }
+    }
+
+    // Метод для перевірки, чи введено коректну опцію
+    private static boolean isValidOption(String input) {
+        for (String option : OPTIONS) {
+            if (option.equals(input)) return true;
+        }
+        return false;
+    }
+
+    // Метод для визначення результату гри
+    private static String getResult(String user, String computer) {
+        if (user.equals(computer)) return "draw";
+
+        if ((user.equals("rock") && computer.equals("scissors")) ||
+                (user.equals("scissors") && computer.equals("paper")) ||
+                (user.equals("paper") && computer.equals("rock"))) {
+            return "win";
+        }
+        return "lose";
+    }
+
+    // Метод для завантаження рейтингу користувача з файлу
+    private static int loadRating(String name) {
+        int rating = 0;
+        File file = new File("rating.txt");
+
+        try (Scanner fileScanner = new Scanner(file)) {
+            while (fileScanner.hasNext()) {
+                String player = fileScanner.next();
+                int score = fileScanner.nextInt();
+                if (player.equals(name)) {
+                    rating = score;
+                    break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+
+        }
+
+        return rating;
     }
 }
