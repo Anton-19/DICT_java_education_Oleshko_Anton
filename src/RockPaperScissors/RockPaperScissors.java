a076569ff22016e2fe8198fc6f16a39e0ccccfb8
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2025-11-11 10:43:03 +0200

    RockPaperScissors 2-th stage

--- src/RockPaperScissors/RockPaperScissors.java
+++ src/RockPaperScissors/RockPaperScissors.java
@@ -1,31 +1,48 @@
 package RockPaperScissors;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        String[] options = {"rock", "paper", "scissors"};
 
         System.out.print("> ");
         String userChoice = scanner.nextLine().toLowerCase();
 
-        String computerChoice = "";
-
-        switch (userChoice) {
-            case "rock":
-                computerChoice = "paper";
-                break;
-            case "paper":
-                computerChoice = "scissors";
-                break;
-            case "scissors":
-                computerChoice = "rock";
-                break;
-            default:
-                System.out.println("Invalid input. Please enter rock, paper, or scissors.");
-                return;
+        if (!userChoice.equals("rock") && !userChoice.equals("paper") && !userChoice.equals("scissors")) {
+            System.out.println("Invalid input. Please enter rock, paper, or scissors.");
+            return;
         }
 
-        System.out.println("Sorry, but the computer chose " + computerChoice);
+        String computerChoice = options[random.nextInt(3)];
+
+        String result;
+        if (userChoice.equals(computerChoice)) {
+            result = "draw";
+        } else if (
+                (userChoice.equals("rock") && computerChoice.equals("scissors")) ||
+                        (userChoice.equals("scissors") && computerChoice.equals("paper")) ||
+                        (userChoice.equals("paper") && computerChoice.equals("rock"))
+        ) {
+            result = "win";
+        } else {
+            result = "lose";
+        }
+
+        switch (result) {
+            case "win":
+                System.out.println("Well done. The computer chose " + computerChoice + " and failed");
+                break;
+            case "draw":
+                System.out.println("There is a draw (" + computerChoice + ")");
+                break;
+            case "lose":
+                System.out.println("Sorry, but the computer chose " + computerChoice);
+                break;
+        }
     }
 }
